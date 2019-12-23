eba0add0e2a1ba930ba57ab1e12fc7eb93a2f7ea
zhangchao <zhangchao@qq.com>
2019-12-23 11:24:01 +0800

    t

---
+++ src/views/shopping/group/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrap">
+    <div class="app-container container">
+      <div class="filter-container title">
+        <div class="btn">
+          <el-button
+            class="filter-item"
+            type="primary"
+          >折叠全部</el-button>
+          <el-button
+            class="filter-item"
+            type="primary"
+          >新增一级分组</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrap{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: calc(100% - 55px);
+  .container{
+    flex: 1;
+    overflow-y: scroll;
+    padding: 16px;
+    background: #f1f4f6;
+    border-radius: 10px;
+    margin: 0 16px;
+    .title{
+      padding: 16px;
+      background: #fff;
+      border-radius: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .btn{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+    }
+    .table{
+      width: 100%;
+      margin-top:12px;
+      border-radius:10px;
+    }
+    .el-table, .el-table__expanded-cell{
+      background: none;
+      width: 100%
+    }
+  }
+  .container::-webkit-scrollbar {
+    display:none
+  }
+}
+</style>
