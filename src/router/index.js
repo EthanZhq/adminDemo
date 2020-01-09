4a2c9388cf124039558d760a2f51bb4299aa358c
lixu <1377182818@qq.com>
2020-01-09 14:09:43 +0800

    代码模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
 
 /* Layout */
-import Layout from "@/layout";
+import Layout from '@/layout'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,264 +32,262 @@ import Layout from "@/layout";
  */
 export const constantRoutes = [
   {
-    path: "/login",
-    component: () => import("@/views/login/index"),
+    path: '/login',
+    component: () => import('@/views/login/index'),
     hidden: true
   },
 
   {
-    path: "/404",
-    component: () => import("@/views/404"),
+    path: '/404',
+    component: () => import('@/views/404'),
     hidden: true
   },
 
   {
-    path: "/",
+    path: '/',
     component: Layout,
-    redirect: "/dashboard",
+    redirect: '/dashboard',
     children: [
       {
-        path: "dashboard",
-        name: "Dashboard",
-        component: () => import("@/views/dashboard/index"),
-        meta: { title: "概况", icon: "dashboard" }
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: '概况', icon: 'dashboard' }
       }
     ]
   },
 
   {
-    path: "/example",
+    path: '/example',
     component: Layout,
-    redirect: "/example/table",
-    name: "example",
-    meta: { title: "店铺12345678", icon: "store" },
+    redirect: '/example/table',
+    name: 'example',
+    meta: { title: '店铺12345678', icon: 'store' },
     children: [
       {
-        path: "table",
-        name: "表格",
-        component: () => import("@/views/table/index"),
-        meta: { title: "首页装修" }
+        path: 'table',
+        name: '表格',
+        component: () => import('@/views/table/index'),
+        meta: { title: '首页装修' }
       },
       {
-        path: "tree",
-        name: "Tree",
-        component: () => import("@/views/tree/index"),
-        meta: { title: "风格配色", roles: ["editor"] }
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: '风格配色', roles: ['editor'] }
       }
     ]
   },
   {
-    path: "/shopping",
+    path: '/shopping',
     component: Layout,
-    redirect: "/shopping/shopping-list",
-    name: "Shopping",
-    meta: { title: "商品", icon: "shopping-bag" },
+    redirect: '/shopping/shopping-list',
+    name: 'Shopping',
+    meta: { title: '商品', icon: 'shopping-bag' },
     children: [
       {
-        path: "shopping-list",
-        name: "shopping-list",
-        component: () => import("@/views/shopping/shopping-list/index"),
-        meta: { title: "商品列表" }
+        path: 'shopping-list',
+        name: 'shopping-list',
+        component: () => import('@/views/shopping/shopping-list/index'),
+        meta: { title: '商品列表' }
       },
       {
-        path: "/shopping-list/add",
-        name: "add",
-        component: () => import("@/views/shopping/shopping-list/add/index"),
-        meta: { title: "添加" },
+        path: '/shopping-list/add',
+        name: 'add',
+        component: () => import('@/views/shopping/shopping-list/add/index'),
+        meta: { title: '添加' },
         hidden: true
       },
       {
-        path: "group",
-        name: "group",
-        component: () => import("@/views/shopping/group/index"),
-        meta: { title: "商品分组" }
+        path: 'group',
+        name: 'group',
+        component: () => import('@/views/shopping/group/index'),
+        meta: { title: '商品分组' }
       },
       {
-        path: "tags",
-        name: "tags",
-        component: () => import("@/views/shopping/tags/index"),
-        meta: { title: "商品标签" }
+        path: 'tags',
+        name: 'tags',
+        component: () => import('@/views/shopping/tags/index'),
+        meta: { title: '商品标签' }
       },
       {
-        path: "comment",
-        name: "comment",
-        component: () => import("@/views/shopping/comment/index"),
-        meta: { title: "商品评论" }
+        path: 'comment',
+        name: 'comment',
+        component: () => import('@/views/shopping/comment/index'),
+        meta: { title: '商品评论' }
       },
       {
-        path: "/comment/detail/",
-        name: "detail",
-        component: () => import("@/views/shopping/comment/detail/index"),
-        meta: { title: "详情" },
+        path: '/comment/detail/',
+        name: 'detail',
+        component: () => import('@/views/shopping/comment/detail/index'),
+        meta: { title: '详情' },
         hidden: true
       },
       {
-        path: "/shopping-list/creat/",
-        name: "creat",
-        component: () => import("@/views/shopping/shopping-list/creat/index"),
-        meta: { title: "创建评论" },
-        hidden: true
-      }
-    ]
-  },
-
-  {
-    path: "/order",
-    component: Layout,
-    meta: { title: "订单", icon: "form" },
-    children: [
-      {
-        path: "all-order",
-        name: "all-order",
-        component: () => import("@/views/order/all-order/index"),
-        meta: { title: "全部订单" }
-      },
-      {
-        path: "/all-order/detail/",
-        name: "detail",
-        component: () => import("@/views/order/all-order/detail/index"),
-        meta: { title: "订单详情" },
-        hidden: true
-      },
-      {
-        path: "/all-order/demolition-order",
-        name: "detail",
-        component: () =>
-          import("@/views/order/all-order/demolition-order/index"),
-        meta: { title: "拆单" },
-        hidden: true
-      },
-      {
-        path: "delivery-management",
-        name: "delivery-management",
-        component: () => import("@/views/order/delivery-management/index"),
-        meta: { title: "发货管理" }
-      },
-      {
-        path: "after-sales-order",
-        name: "after-sales-order",
-        component: () => import("@/views/order/after-sales-order/index"),
-        meta: { title: "售后订单" }
-      },
-      {
-        path: "/after-sales-order/details/:id(\\d+)",
-        name: "details",
-        component: () =>
-          import("@/views/order/after-sales-order/details/index"),
-        meta: { title: "订单详情" },
+        path: '/shopping-list/creat/',
+        name: 'creat',
+        component: () => import('@/views/shopping/shopping-list/creat/index'),
+        meta: { title: '创建评论' },
         hidden: true
       }
     ]
   },
 
   {
-    path: "/member",
+    path: '/order',
     component: Layout,
-    redirect: "/member/memberList",
-    name: "member",
-    meta: { title: "会员", icon: "vip" },
+    meta: { title: '订单', icon: 'form' },
     children: [
       {
-        path: "memberList",
-        name: "memberList",
-        component: () => import("@/views/member/memberList/index"),
-        meta: { title: "会员列表" }
+        path: 'all-order',
+        name: 'all-order',
+        component: () => import('@/views/order/all-order/index'),
+        meta: { title: '全部订单' }
       },
       {
-        path: "/memberList/detail",
-        name: "detail",
-        component: () => import("@/views/member/memberList/detail/index"),
-        meta: { title: "会员详情" },
+        path: '/all-order/detail/',
+        name: 'detail',
+        component: () => import('@/views/order/all-order/detail/index'),
+        meta: { title: '订单详情' },
         hidden: true
       },
       {
-        path: "memberGrade",
-        name: "MemberGrade",
-        component: () => import("@/views/member/memberGrade/index"),
-        meta: { title: "等级配置" }
-      },
-      {
-        path: "/memberGrade/add",
-        name: "add",
-        component: () => import("@/views/member/memberGrade/add/index"),
-        meta: { title: "会员等级" },
+        path: '/all-order/demolition-order',
+        name: 'detail',
+        component: () => import('@/views/order/all-order/demolition-order/index'),
+        meta: { title: '拆单' },
         hidden: true
       },
       {
-        path: "memberIntegral",
-        name: "memberIntegral",
-        component: () => import("@/views/member/memberIntegral/index"),
-        meta: { title: "积分配置" }
+        path: 'delivery-management',
+        name: 'delivery-management',
+        component: () => import('@/views/order/delivery-management/index'),
+        meta: { title: '发货管理' }
+      },
+      {
+        path: 'after-sales-order',
+        name: 'after-sales-order',
+        component: () => import('@/views/order/after-sales-order/index'),
+        meta: { title: '售后订单' }
+      },
+      {
+        path: '/after-sales-order/details/:id(\\d+)',
+        name: 'details',
+        component: () => import('@/views/order/after-sales-order/details/index'),
+        meta: { title: '订单详情' },
+        hidden: true
+      }
+    ]
+  },
+
+  {
+    path: '/member',
+    component: Layout,
+    redirect: '/member/memberList',
+    name: 'member',
+    meta: { title: '会员', icon: 'vip' },
+    children: [
+      {
+        path: 'memberList',
+        name: 'memberList',
+        component: () => import('@/views/member/memberList/index'),
+        meta: { title: '会员列表' }
+      },
+      {
+        path: '/memberList/detail',
+        name: 'detail',
+        component: () => import('@/views/member/memberList/detail/index'),
+        meta: { title: '会员详情' },
+        hidden: true
+      },
+      {
+        path: 'memberGrade',
+        name: 'MemberGrade',
+        component: () => import('@/views/member/memberGrade/index'),
+        meta: { title: '等级配置' }
+      },
+      {
+        path: '/memberGrade/add',
+        name: 'add',
+        component: () => import('@/views/member/memberGrade/add/index'),
+        meta: { title: '会员等级' },
+        hidden: true
+      },
+      {
+        path: 'memberIntegral',
+        name: 'memberIntegral',
+        component: () => import('@/views/member/memberIntegral/index'),
+        meta: { title: '积分配置' }
       }
     ]
   },
   {
-    path: "/assets",
+    path: '/assets',
     component: Layout,
-    redirect: "/assets/storeInfo",
-    name: "Assets",
-    meta: { title: "资产", icon: "dollar" },
+    redirect: '/assets/storeInfo',
+    name: 'Assets',
+    meta: { title: '资产', icon: 'dollar' },
     children: [
       {
-        path: "assets",
-        name: "Assets",
-        component: () => import("@/views/assets/index"),
-        meta: { title: "资产" }
+        path: 'assets',
+        name: 'Assets',
+        component: () => import('@/views/assets/index'),
+        meta: { title: '资产' }
       }
     ]
   },
   {
-    path: "/setUp",
+    path: '/setUp',
     component: Layout,
-    redirect: "/setUp/storeInfo",
-    name: "SetUp",
-    meta: { title: "设置", icon: "set_up" },
+    redirect: '/setUp/storeInfo',
+    name: 'SetUp',
+    meta: { title: '设置', icon: 'set_up' },
     children: [
       {
-        path: "storeInfo",
-        name: "StoreInfo",
-        component: () => import("@/views/setUp/storeInfo/index"),
-        meta: { title: "店铺信息" }
+        path: 'storeInfo',
+        name: 'StoreInfo',
+        component: () => import('@/views/setUp/storeInfo/index'),
+        meta: { title: '店铺信息' }
       },
       {
-        path: "express-management",
-        name: "express-management",
-        component: () => import("@/views/setUp/express-management/index"),
-        meta: { title: "快递管理" }
+        path: 'express-management',
+        name: 'express-management',
+        component: () => import('@/views/setUp/express-management/index'),
+        meta: { title: '快递管理' }
       },
       {
-        path: "/express-management/add",
-        name: "add",
-        component: () => import("@/views/setUp/express-management/add/index"),
-        meta: { title: "添加快递公司" },
+        path: '/express-management/add',
+        name: 'add',
+        component: () => import('@/views/setUp/express-management/add/index'),
+        meta: { title: '添加快递公司' },
         hidden: true
       },
       {
-        path: "generalSettings",
-        name: "GeneralSettings",
-        component: () => import("@/views/setUp/generalSettings/index"),
-        meta: { title: "通用设置" }
+        path: 'generalSettings',
+        name: 'GeneralSettings',
+        component: () => import('@/views/setUp/generalSettings/index'),
+        meta: { title: '通用设置' }
       }
     ]
   },
 
   // 404 page must be placed at the end !!!
-  { path: "*", redirect: "/404", hidden: true }
-];
+  { path: '*', redirect: '/404', hidden: true }
+]
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
     routes: constantRoutes
-  });
+  })
 
-const router = createRouter();
+const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter();
-  router.matcher = newRouter.matcher; // reset router
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
 }
 
-export default router;
+export default router
