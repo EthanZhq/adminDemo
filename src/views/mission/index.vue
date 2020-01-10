21e19c706e17c813379a789f81874776b01e0fac
zhangdoudou <3099345774@qq.com>
2020-01-10 10:28:50 +0800

    111

--- src/views/mission/index.vue
+++ src/views/mission/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <DecLeft @on-add-component="addComponent"/>
-    <DecView :components="components" @on-change-list="changeComponents"/>
-    <DecRight :currentComponent="components.list[components.current]" @on-change-list="changeComponents"/>
+    <DecLeft @on-add-component="addComponent" />
+    <DecView :components="components" @on-change-list="changeComponents" />
+    <DecRight
+      :current-component="components.list[components.current]"
+      @on-change-list="changeComponents"
+    />
   </div>
 </template>
 
@@ -11,65 +14,89 @@ import DecLeft from '@/components/decoration/dec-left'
 import DecRight from '@/components/decoration/dec-right'
 import DecView from '@/components/decoration/dec-view'
 export default {
-  components:{
+  components: {
     DecLeft,
     DecView,
     DecRight
   },
   data() {
     return {
-      components:{
-        current:0,
-        list:[
+      components: {
+        current: 0,
+        list: [
           {
-            name:"Search",
-            title:'搜索模块',
-            pic:require('../../assets/decoration/search.png'),
-            type:0,
-            options:{color:"#e4e4e4",bgColor:'#E02E24',placeholder:'搜索店铺内商品',border:'0',textAlign:'left'}
+            name: 'Search',
+            title: '搜索模块',
+            pic: require('../../assets/decoration/search.png'),
+            type: 0,
+            options: {
+              color: '#e4e4e4',
+              bgColor: '#E02E24',
+              placeholder: '搜索店铺内商品',
+              border: '0',
+              textAlign: 'left'
+            }
           },
           {
-            name:"Banner",
-            title:'轮播图片',
-            pic:require('../../assets/decoration/pic.gif'),
-            type:0,
-            options:{interval:3,imgList:[{pic:require('../../assets/decoration/banner.jpg')}]}
+            name: 'Banner',
+            title: '轮播图片',
+            pic: require('../../assets/decoration/pic.gif'),
+            type: 0,
+            options: {
+              interval: 3,
+              imgList: [{ pic: require('../../assets/decoration/banner.jpg') }]
+            }
           },
           {
-            name:"Nav",
-            title:'店铺导航',
-            pic:require('../../assets/decoration/nav.png'),
-            type:0,
-            options:{navs:[{name:'断码清仓',pic:require('../../assets/decoration/pic.gif'),color:'#555'},{name:'全球购',pic:require('../../assets/decoration/pic.gif'),color:'#555'},{name:'幸运砸金蛋',pic:require('../../assets/decoration/pic.gif'),color:'#555'},{name:'潮人穿搭',pic:require('../../assets/decoration/pic.gif'),color:'#555'}]}
+            name: 'Nav',
+            title: '店铺导航',
+            pic: require('../../assets/decoration/nav.png'),
+            type: 0,
+            options: {
+              navs: [
+                {
+                  name: '断码清仓',
+                  pic: require('../../assets/decoration/pic.gif'),
+                  color: '#555'
+                },
+                { name: '全球购', pic: require('../../assets/decoration/pic.gif'), color: '#555' },
+                {
+                  name: '幸运砸金蛋',
+                  pic: require('../../assets/decoration/pic.gif'),
+                  color: '#555'
+                },
+                { name: '潮人穿搭', pic: require('../../assets/decoration/pic.gif'), color: '#555' }
+              ]
+            }
           }
         ]
       }
     }
   },
   methods: {
-    addComponent(e){
+    addComponent(e) {
       console.log(e)
-      let component=JSON.parse(JSON.stringify(e))
+      const component = JSON.parse(JSON.stringify(e))
       this.components.list.unshift(component)
-      this.components.current=0
-      console.log('添加'+this.components.list)
+      this.components.current = 0
+      console.log('添加' + this.components.list)
     },
-    changeComponents(e){
-      if(typeof(e)=='number'||typeof(e)=='string'){
-        this.components.current=e
-      }else if(e instanceof Array){
-        this.components.list=e
-        console.log('修改'+this.components.list)
-      }else if(typeof(e)=='object'){
-        this.components.list[this.components.current].options=e
-        console.log('修改'+this.components.list)
+    changeComponents(e) {
+      if (typeof e === 'number' || typeof e === 'string') {
+        this.components.current = e
+      } else if (e instanceof Array) {
+        this.components.list = e
+        console.log('修改' + this.components.list)
+      } else if (typeof e === 'object') {
+        this.components.list[this.components.current].options = e
+        console.log('修改' + this.components.list)
       }
     }
   }
 }
 </script>
 <style scoped lang="scss">
-.container{
+.container {
   display: flex;
   padding: 20px 30px;
 }
--- src/views/setUp/users/index.vue
+++ src/views/setUp/users/index.vue
@@ -1,118 +1,79 @@
 <template>
   <div class="wrap">
     <div class="app-container container">
-       <div class="add">
-          <h3>订单流程设置</h3>
-           <div class="express-box">
-            <span class="express-title">未支付订单</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">分钟后，自动关闭</span>
-          </div>
-          <div class="express-box">
-            <span class="express-title">已发货订单</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，自动确认收货</span>
-          </div>
-          <div class="express-box">
-            <span class="express-title">已收货订单</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，关闭申请售后功能</span>
-            <span class="tips">最长可设置365天，0代表买家在确认收货后无法申请售后</span>
-          </div>
+      <div class="add">
+        <h3>订单流程设置</h3>
+        <div class="express-box">
+          <span class="express-title">未支付订单</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">分钟后，自动关闭</span>
         </div>
-        <div class="add">
-          <h3>订单流程设置</h3>
-           <div class="express-box">
-            <span class="express-titles">买家发起退款申请</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，商家未处理，系统将自动拒绝退款</span>
-          </div>
-          <div class="express-box">
-            <span class="express-titles">买家发起退货退款申请</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，商家未处理，系统将自动拒绝退货退款</span>
-          </div>
-          <div class="express-box">
-            <span class="express-titles">买家发起换货申请</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，商家未处理，系统将自动拒绝换货退款</span>
-          </div>
-          <div class="express-box">
-            <span class="express-titles">买家已退货</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，商家未处理，系统将自动拒绝退款</span>
-          </div>
-          <div class="express-box">
-            <span class="express-titles">商家同意退货</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，买家未处理，系统将自动拒绝售后</span>
-          </div>
-           <div class="express-box explain">
-            <span class="express-titles">说明：</span>
-            <div class="explain-main">
-              1、系统默认的时间为7天，手动设置时间最长不超过90天。
-            </div>
-          </div>
+        <div class="express-box">
+          <span class="express-title">已发货订单</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，自动确认收货</span>
         </div>
-        <div class="add">
-          <h3>评论设置</h3>
-          <div class="express-box">
-            <span class="express-title">买家交易成功</span>
-            <el-input
-              placeholder=""
-              style="width: 120px;"
-              class="filter-item"
-            />
-            <span class="close">天后，买家未评论，系统将自动评论</span>
-            <span class="tips">系统默认的时间为7天，手动设置时间最长不超过90天</span>
-          </div>
-          <div class="express-box">
-            <span class="express-title" style="width:120px">自动评论内容：</span>
-            <el-input type="textarea" placeholder="用户评论超时，系统自动好评" :autosize="{ minRows: 1, maxRows: 20}" style="width:40%"></el-input>
-          </div>
-          <div class="btn-box">
-            <el-button
-              class="filter-item"
-              type="primary"
-            >确定</el-button>
-            <el-button
-              class="filter-item"
-              type="primary"
-            >取消</el-button>
-          </div>
+        <div class="express-box">
+          <span class="express-title">已收货订单</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，关闭申请售后功能</span>
+          <span class="tips">最长可设置365天，0代表买家在确认收货后无法申请售后</span>
         </div>
+      </div>
+      <div class="add">
+        <h3>订单流程设置</h3>
+        <div class="express-box">
+          <span class="express-titles">买家发起退款申请</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，商家未处理，系统将自动拒绝退款</span>
+        </div>
+        <div class="express-box">
+          <span class="express-titles">买家发起退货退款申请</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，商家未处理，系统将自动拒绝退货退款</span>
+        </div>
+        <div class="express-box">
+          <span class="express-titles">买家发起换货申请</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，商家未处理，系统将自动拒绝换货退款</span>
+        </div>
+        <div class="express-box">
+          <span class="express-titles">买家已退货</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，商家未处理，系统将自动拒绝退款</span>
+        </div>
+        <div class="express-box">
+          <span class="express-titles">商家同意退货</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，买家未处理，系统将自动拒绝售后</span>
+        </div>
+        <div class="express-box explain">
+          <span class="express-titles">说明：</span>
+          <div class="explain-main">1、系统默认的时间为7天，手动设置时间最长不超过90天。</div>
+        </div>
+      </div>
+      <div class="add">
+        <h3>评论设置</h3>
+        <div class="express-box">
+          <span class="express-title">买家交易成功</span>
+          <el-input placeholder style="width: 120px;" class="filter-item" />
+          <span class="close">天后，买家未评论，系统将自动评论</span>
+          <span class="tips">系统默认的时间为7天，手动设置时间最长不超过90天</span>
+        </div>
+        <div class="express-box">
+          <span class="express-title" style="width:120px">自动评论内容：</span>
+          <el-input
+            type="textarea"
+            placeholder="用户评论超时，系统自动好评"
+            :autosize="{ minRows: 1, maxRows: 20}"
+            style="width:40%"
+          ></el-input>
+        </div>
+        <div class="btn-box">
+          <el-button class="filter-item" type="primary">确定</el-button>
+          <el-button class="filter-item" type="primary">取消</el-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -120,80 +81,76 @@
 <script>
 export default {
   data() {
-    return {
-
-    }
+    return {}
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 <style lang="scss" scoped>
-.wrap{
+.wrap {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   height: calc(100% - 55px);
-  .container{
+  .container {
     flex: 1;
     overflow-y: scroll;
     padding: 16px;
     background: #f1f4f6;
     border-radius: 10px;
     margin: 0 16px 16px;
-    .add{
+    .add {
       background: #fff;
       padding: 0 32px;
-      h3{
+      h3 {
         padding: 32px 0 0;
-        margin: 0
+        margin: 0;
       }
-      .express-box{
+      .express-box {
         padding: 24px 0 0 24px;
         display: flex;
         align-items: center;
         color: #666;
-        .express-title{
+        .express-title {
           font-size: 14px;
           padding-right: 12px;
           width: 100px;
-          display: block
+          display: block;
         }
-        .express-titles{
+        .express-titles {
           font-size: 14px;
           padding-right: 12px;
           width: 160px;
-          display: block
+          display: block;
         }
-        .close{
+        .close {
           font-size: 14px;
           padding-left: 32px;
         }
-        .tips{
+        .tips {
           color: #999;
           padding-left: 16px;
-          font-size: 12px
+          font-size: 12px;
         }
       }
 
-      .dec{
+      .dec {
         display: flex;
         align-items: flex-start;
-        .area{
+        .area {
           width: 50%;
         }
       }
-      .explain{
+      .explain {
         color: #999;
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        .explain-main{
-          padding-top: 16px
+        .explain-main {
+          padding-top: 16px;
         }
       }
-      .btn-box{
+      .btn-box {
         padding: 60px 0;
         display: flex;
         align-items: center;
@@ -202,7 +159,7 @@ export default {
     }
   }
   .container::-webkit-scrollbar {
-    display:none
+    display: none;
   }
 }
 </style>
